8a87fa6f0de126bd49153231ad433e5f98783858
Zinhle144 <zinhlemasango88@gmail.com>
2023-06-18 17:40:07 -0700

    o

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 97; i < 123; i++)
 	{
-	if (i != 101 && i != 113)
+		if (i != 101 && i != 113)
 		{
 			putchar(i);
 		}
@@ -21,4 +21,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-
